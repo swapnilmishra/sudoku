45aa639c9bba6dcd1cd2a69b6f05c73a51de2d86
Swapnil Mishra <swapmis1084@gmail.com>
2016-12-04 16:04:14 +0530

    Removing index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import thunkMiddleware from 'redux-thunk'
 import { createStore,applyMiddleware,combineReducers,compose } from 'redux'
 import { sudokuReducer,snackBarReducer,loaderReducer,sudokuErrorReducer,commonState} from './reducers'
